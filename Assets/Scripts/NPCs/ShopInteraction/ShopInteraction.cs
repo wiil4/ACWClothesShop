1579e07e46e627a7ef57c0cb4daf7be9b8f7f05c
Wilfor Ibanez <aln.wil4@outlook.es>
2023-05-19 22:56:25 -0400

    Blacksmith NPC character added for shopping interaction

---
+++ Assets/Scripts/NPCs/ShopInteraction/ShopInteraction.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopInteraction : MonoBehaviour
+{
+    bool _playerDetected;
+
+    [Header("Character Shop Panel")]
+    [SerializeField] GameObject _shopPanel;
+    
+    void Start()
+    {
+        _playerDetected = false;
+    }
+
+    
+    void Update()
+    {
+        if(_playerDetected)
+        {
+            ShowDialogButton();
+        }
+    }
+
+    private void ShowDialogButton()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            _shopPanel.SetActive(true);
+            //GameManager set playermove to false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _playerDetected = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _playerDetected = false;
+        }
+    }
+}
